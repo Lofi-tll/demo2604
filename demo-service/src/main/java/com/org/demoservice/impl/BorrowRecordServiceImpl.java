8b36afbe28d04cd750cdfe03be93990ee2777732
Lofi_tll <l17800569882@outlook.com>
2026-04-15 02:25:54 +0800

    业务全部完工，细节优化即可

--- demo-service/src/main/java/com/org/demoservice/impl/BorrowRecordServiceImpl.java
+++ demo-service/src/main/java/com/org/demoservice/impl/BorrowRecordServiceImpl.java
@@ -11,6 +11,7 @@ import com.org.demomapper.BookMapper;
 import com.org.demomapper.BorrowRecordMapper;
 import com.org.demoservice.BorrowRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +33,8 @@ public class BorrowRecordServiceImpl extends ServiceImpl<BorrowRecordMapper, Bor
 
     //自注入防止事务失效
     @Autowired
+    //懒加载防止循环依赖报错
+    @Lazy
     private BorrowRecordService self;
 
     BorrowRecordServiceImpl(BorrowRecordMapper borrowRecordMapper, BookMapper bookMapper) {
